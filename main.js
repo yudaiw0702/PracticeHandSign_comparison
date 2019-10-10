8885af294604d0cb05e633d5bb1b00a5b05f26b5
Yudai Watanabe <b1601824@planet.kanazawa-it.ac.jp>
2019-10-10 20:04:50 +0900

    リファクタリング＋データ拡張

--- main.js
+++ main.js
@@ -6,66 +6,64 @@ $(function() {
   function a (evt) {
     $("#sign_temp").text("あなたの回答：あ");
     if(stage == 'あ'){
-      $('.circle').css('display','inline');
-      setTimeout(function(){
-        $('.handsign_img').attr("src","image/handsign/hs_i.jpg");
-        $(".circle").css('display','none');
-        $(".handsign_text").text("問題：い");
-        $("#sign_temp").text("あなたの回答：？");
-        stage = 'い';
-      }, 2000);
+      call_a();
     }
   }
 
   function i (evt) {
     $("#sign_temp").text("あなたの回答：い");
     if(stage == 'い'){
-      $('.circle').css('display','inline');
-      setTimeout(function(){
-        $('.handsign_img').attr("src","image/handsign/hs_ku.jpg");
-        $(".circle").css('display','none');
-        $(".handsign_text").text("問題：く");
-        $("#sign_temp").text("あなたの回答：？");
-        stage = 'く';
-      }, 2000);
+      call_i();
     }
   }
 
   function ku (evt) {
     $("#sign_temp").text("あなたの回答：く");
     if(stage == 'く'){
-      $('.circle').css('display','inline');
-      setTimeout(function(){
-        $('.handsign_img').attr("src","image/handsign/hs_si.jpg");
-        $(".circle").css('display','none');
-        $(".handsign_text").text("問題：し");
-        $("#sign_temp").text("あなたの回答：？");
-        stage = 'し';
-      }, 2000);
+      call_ku();
     }
   }
 
   function si (evt) {
     $("#sign_temp").text("あなたの回答：し");
     if(stage == 'し'){
-      $('.circle').css('display','inline');
-      setTimeout(function(){
-        $('.handsign_img').attr("src","image/handsign/hs_a.jpg");
-        $(".circle").css('display','none');
-        $(".handsign_text").text("問題：あ");
-        $("#sign_temp").text("あなたの回答：？");
-        stage = 'あ';
-      }, 2000);
+      call_si();
     }
   }
 
+  function e (evt) {
+    $("#sign_temp").text("あなたの回答：え");
+    if(stage == 'え'){
+      call_e();
+    }
+  }
+
+  function ru (evt) {
+    $("#sign_temp").text("あなたの回答：る");
+    if(stage == 'る'){
+      $('.circle').css('display','inline');
+        call_ru();
+    }
+  }
+
+  function mu (evt) {
+    $("#sign_temp").text("あなたの回答：む");
+    if(stage == 'む'){
+      call_mu();
+    }
+  }
+
+  function hi (evt) {
+    $("#sign_temp").text("あなたの回答：ひ");
+    if(stage == 'ひ'){
+      call_hi();
+    }
+  }
+
+  //問題変更ボタン
   $('.btn-change').on('click', function(){
     if(stage == 'あ'){
-      $('.handsign_img').attr("src","image/handsign/hs_i.jpg");
-      $(".circle").css('display','none');
-      $(".handsign_text").text("問題：い");
-      $("#sign_temp").text("あなたの回答：？");
-      stage = 'い';
+      call_a();
     };
   });
 
@@ -82,6 +80,94 @@ $(function() {
     };
   });
 
+  function call_a(){
+    $('.circle').css('display','inline');
+    setTimeout(function(){
+      $('.handsign_img').attr("src","image/handsign/hs_i.jpg");
+      $(".circle").css('display','none');
+      $(".handsign_text").text("問題：い");
+      $("#sign_temp").text("あなたの回答：？");
+      stage = 'い';
+    }, 2000);
+  };
+
+  function call_i(){
+    $('.circle').css('display','inline');
+    setTimeout(function(){
+      $('.handsign_img').attr("src","image/handsign/hs_ku.jpg");
+      $(".circle").css('display','none');
+      $(".handsign_text").text("問題：く");
+      $("#sign_temp").text("あなたの回答：？");
+      stage = 'く';
+    }, 2000);
+  };
+
+  function call_ku(){
+    $('.circle').css('display','inline');
+    setTimeout(function(){
+      $('.handsign_img').attr("src","image/handsign/hs_si.jpg");
+      $(".circle").css('display','none');
+      $(".handsign_text").text("問題：し");
+      $("#sign_temp").text("あなたの回答：？");
+      stage = 'し';
+    }, 2000);
+  };
+
+  function call_si(){
+    $('.circle').css('display','inline');
+    setTimeout(function(){
+      $('.handsign_img').attr("src","image/handsign/hs_a.jpg");
+      $(".circle").css('display','none');
+      $(".handsign_text").text("問題：え");
+      $("#sign_temp").text("あなたの回答：？");
+      stage = 'え';
+    }, 2000);
+  };
+
+  function call_e(){
+    $('.circle').css('display','inline');
+    setTimeout(function(){
+      $('.handsign_img').attr("src","image/handsign/hs_ru.jpg");
+      $(".circle").css('display','none');
+      $(".handsign_text").text("問題：る");
+      $("#sign_temp").text("あなたの回答：？");
+      stage = 'る';
+    }, 2000);
+  };
+
+  function call_ru(){
+    $('.circle').css('display','inline');
+    setTimeout(function(){
+      $('.handsign_img').attr("src","image/handsign/hs_mu.jpg");
+      $(".circle").css('display','none');
+      $(".handsign_text").text("問題：む");
+      $("#sign_temp").text("あなたの回答：？");
+      stage = 'む';
+    }, 2000);
+  };
+
+  function call_mu(){
+    $('.circle').css('display','inline');
+    setTimeout(function(){
+      $('.handsign_img').attr("src","image/handsign/hs_hi.jpg");
+      $(".circle").css('display','none');
+      $(".handsign_text").text("問題：ひ");
+      $("#sign_temp").text("あなたの回答：？");
+      stage = 'ひ';
+    }, 2000);
+  };
+
+  function call_hi(){
+    setTimeout(function(){
+      $('.circle').css('display','inline');
+      $('.handsign_img').attr("src","image/handsign/hs_a.jpg");
+      $(".circle").css('display','none');
+      $(".handsign_text").text("問題：あ");
+      $("#sign_temp").text("あなたの回答：？");
+      stage = 'あ';
+    }, 2000);
+  };
+
   //指文字データ
   var trainer = new LeapTrainer.Controller();
 
@@ -93,11 +179,22 @@ $(function() {
 
   trainer.fromJSON('{"name":"SI","pose":true,"data":[[{"x":0.24692425376236726,"y":-0.03363751060968054,"z":-0.03368873540298582,"stroke":1},{"x":0.2078207089603945,"y":0.005401374656955393,"z":-0.004025538024756012,"stroke":1},{"x":0.16871716415842186,"y":0.0444402599235912,"z":0.0256376593534738,"stroke":1},{"x":0.1296136193564491,"y":0.08347914519022699,"z":0.05530085673170361,"stroke":1},{"x":-0.7530757462376327,"y":-0.09968326916109303,"z":-0.043224242657435574,"stroke":1}]]}');
   
+  trainer.fromJSON('{"name":"E","pose":true,"data":[[{"x":0.04332285781618256,"y":0.31481125304863133,"z":0.1187568035944021,"stroke":1},{"x":-0.07676560402506163,"y":0.21913520884143844,"z":0.12499072356232843,"stroke":1},{"x":-0.19685406586630586,"y":0.12345916463424567,"z":0.13122464353025487,"stroke":1},{"x":-0.31694252770755005,"y":0.027783120427052777,"z":0.1374585634981812,"stroke":1},{"x":0.547239339782735,"y":-0.6851887469513687,"z":-0.5124307341851665,"stroke":1}]]}');
+
+  trainer.fromJSON('{"name":"RU","pose":true,"data":[[{"x":0.2926610908293195,"y":0.24752671135559362,"z":0.09356960793646493,"stroke":1},{"x":0.14022970903905768,"y":0.16762006826919434,"z":0.04106326227213847,"stroke":1},{"x":-0.012201672751204151,"y":0.08771342518279507,"z":-0.01144308339218797,"stroke":1},{"x":-0.16463305454146587,"y":0.007806782096395737,"z":-0.06394942905651448,"stroke":1},{"x":-0.3170644363317277,"y":-0.07209986099000354,"z":-0.11645577472084094,"stroke":1},{"x":-0.46949581812198954,"y":-0.15200650407640262,"z":-0.16896212038516742,"stroke":1},{"x":0.5305041818780105,"y":-0.2865606218375724,"z":0.22617753734610743,"stroke":1}]]}');
+  
+  trainer.fromJSON('{"name":"MU","pose":true,"data":[[{"x":0.10174123352196018,"y":-0.27509655001032374,"z":0.26054674104851927,"stroke":1},{"x":0.14673347987352692,"y":-0.24163448332989212,"z":0.12073725035821764,"stroke":1},{"x":0.19172572622509376,"y":-0.2081724166494605,"z":-0.019072240332084045,"stroke":1},{"x":-0.4402004396205811,"y":0.7249034499896763,"z":-0.362211751074653,"stroke":1}]]}');
+  
+  trainer.fromJSON('{"name":"HI","pose":true,"data":[[{"x":0.10987786831137547,"y":0.13298039446915183,"z":0.3472222222222222,"stroke":1},{"x":0.09669252411401041,"y":0.1170227471328536,"z":0.30555555555555547,"stroke":1},{"x":-0.20657039242538588,"y":-0.25000314160200543,"z":-0.6527777777777778,"stroke":1}]]}');
   //trainer.fromJSON('');
 
   trainer.on('A', a);
   trainer.on('I', i);
   trainer.on('KU', ku);
   trainer.on('SI', si);
+  trainer.on('E', e);
+  trainer.on('RU', ru);
+  trainer.on('MU', mu);
+  trainer.on('HI', hi);
 
 });
