96c34a6ac18f8733a690201d2e0ecdbbcfef0d11
Yudai Watanabe <b1601824@planet.kanazawa-it.ac.jp>
2019-11-15 13:55:13 +0900

    答えの表示を変更

--- main.js
+++ main.js
@@ -84,12 +84,12 @@ $(function() {
   //ボタン表示切り替え
   $('.hide-ans').on('click', function(){
     if(hide_btn_sw == 1){
-      $('.handsign_img').css('display','inline');
-      $('.hide-ans').text('答えを隠す');
+      $('.handsign_img').css('display','none');
+      $('.hide-ans').text('画像を表示');
       hide_btn_sw = 0;
     }else{
-      $('.handsign_img').css('display','none');
-      $('.hide-ans').text('答えを表示');
+      $('.handsign_img').css('display','inline');
+      $('.hide-ans').text('画像を隠す');
       hide_btn_sw = 1;
     };
   });
